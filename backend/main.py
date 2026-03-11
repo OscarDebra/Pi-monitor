bbb02cc34e0e94538a7a73ef100f66be733d7569
OscarDebra <osc.debra@gmail.com>
2026-03-11 10:03:40 +0100

    removed unnecessary stats from backend

--- backend/main.py
+++ backend/main.py
@@ -31,12 +31,8 @@ def get_stats():
     return {
         "cpu_percent": psutil.cpu_percent(interval=1),
         "cpu_temp": get_cpu_temp(),
-        "ram_used": round(ram.used / 1024**2),
         "ram_total": round(ram.total / 1024**2),
         "ram_percent": ram.percent,
         "disk_used": round(disk.used / 1024**3, 1),
         "disk_total": round(disk.total / 1024**3, 1),
-        "disk_percent": disk.percent,
-        "net_sent": round(net.bytes_sent / 1024**2),
-        "net_recv": round(net.bytes_recv / 1024**2),
     }
